0e260d825a8ef9c08e52820a8e389a7b4f0f46ce
kazushige20120727 <e.everyday.e@gmail.com>
2021-11-16 08:03:06 +0900

    ユーザー編集機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def edit
+
+  end
+
+  def update
+    if current_user.update(user_params)
+     redirect_to root_path
+    # ↑pictweetでは投稿完了画面（update.html）に行っていたが、今回はindex.htmlに飛ぶためこの形
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  
+end
